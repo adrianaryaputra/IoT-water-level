8ae7b277cb103c01c3e0fbd8162687c237a08f5d
Adrian A. Firmansyah <adrianaryaputra@icloud.com>
2020-08-26 01:02:23 +0700

    fix empty measurement not throw ValidationError

--- Services/measurement/src/validator/measurement.joi.js
+++ Services/measurement/src/validator/measurement.joi.js
@@ -1,13 +1,32 @@
 const Joi = require('joi');
 
-module.exports = Joi.object({
+const measurementSchema = Joi.object().keys({
+  level: Joi
+    .number()
+    .required()
+    .min(parseInt(process.env.LEVEL_MIN))
+    .max(parseInt(process.env.LEVEL_MAX)),
+
+  temperature: Joi
+    .number()
+    .min(parseInt(process.env.TEMP_MIN))
+    .max(parseInt(process.env.TEMP_MAX)),
+
+  humidity: Joi
+    .number()
+    .min(parseInt(process.env.HUMIDITY_MIN))
+    .max(parseInt(process.env.HUMIDITY_MAX)),
+})
+
+module.exports = Joi.object().keys({
   mac_address: Joi
     .string()
     .required()
     .pattern(
       /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/
     )
-    .replace(/:/g, '-'),
+    .replace(/:/g, '-')
+    .uppercase(),
 
   lifetime: Joi
     .number()
@@ -15,24 +34,6 @@ module.exports = Joi.object({
     .min(parseInt(process.env.UPDATE_MIN))
     .max(parseInt(process.env.UPDATE_MAX)),
 
-  measurement: {
-
-    level: Joi
-      .number()
-      .required()
-      .min(parseInt(process.env.LEVEL_MIN))
-      .max(parseInt(process.env.LEVEL_MAX)),
-
-    temperature: Joi
-      .number()
-      .min(parseInt(process.env.TEMP_MIN))
-      .max(parseInt(process.env.TEMP_MAX)),
-
-    humidity: Joi
-      .number()
-      .min(parseInt(process.env.HUMIDITY_MIN))
-      .max(parseInt(process.env.HUMIDITY_MAX)),
-
-  },
+  measurement: measurementSchema.required(),
   
 });
