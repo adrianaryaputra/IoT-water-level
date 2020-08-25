80827b8600273460d498582188c5fc0af33fed81
Adrian A. Firmansyah <adrianaryaputra@icloud.com>
2020-08-26 01:03:21 +0700

    destructuring http query

--- Services/measurement/src/controller/get-measurement.js
+++ Services/measurement/src/controller/get-measurement.js
@@ -1,4 +1,5 @@
 const validator = require('../validator');
+const {listMeasurement} = require('../use-cases');
 
 module.exports = function({ listMeasurement }){
   return async function getMeasurement(httpRequest){
@@ -7,11 +8,18 @@ module.exports = function({ listMeasurement }){
       'Content-Type': 'application/json',
     }
 
-    const query = validator.validateGetQuery(httpRequest.query);
+    const nonvalidatedQuery = {
+      mac_address: httpRequest.query.mac_address,
+      date_from: new Date(httpRequest.query.date_from),
+      date_to: new Date(httpRequest.query.date_to),
+      limit: parseInt(httpRequest.query.limit),
+    }
+    
+    const validatedQuery = validator.validateGetQuery(nonvalidatedQuery);
 
     try{
 
-      const measurementBody = await listMeasurement(query);
+      const measurementBody = await listMeasurement(validatedQuery);
       return {
         headers: headers,
         statusCode: 200,
