6d569b1620763b3762f15b362b6332c9e369246c
Adrian A. Firmansyah <adrianaryaputra@icloud.com>
2020-08-27 13:58:15 +0700

    🛠 FIX list-measurement

--- Services/measurement/src/use-cases/index.js
+++ Services/measurement/src/use-cases/index.js
@@ -1,8 +1,12 @@
 const {measurementDB} = require('../data-access-mongoose');
-const makeListMeasurement = require('./list-measurement');
 
-const listMeasurement = makeListMeasurement({measurementDB})
+const makeListMeasurement = require('./list-measurement');
+const listMeasurement = makeListMeasurement({measurementDB});
+
+const makeCreateMeasurement = require('./create-measurement');
+const createMeasurement = makeCreateMeasurement({measurementDB});
 
 module.exports = Object.freeze({
   listMeasurement,
+  createMeasurement,
 })
--- Services/measurement/src/use-cases/list-measurement.js
+++ Services/measurement/src/use-cases/list-measurement.js
@@ -2,15 +2,15 @@ module.exports = ({measurementDB}) => {
 
   return async (query) => {
 
-    const db = measurementDB().findAll();
+    const measurement = measurementDB();
 
-    if(query.mac_address) db.findByMac(query.mac_address);
-    if(query.date_from) db.dateFrom(query.date_from);
-    if(query.date_to) db.dateTo(query.date_to);
-    db.sort(-1);
-    if(query.limit) db.limit(query.limit);
+    if(query.mac_address) measurement.findByMac(query.mac_address);
+    if(query.date_from) measurement.dateFrom(query.date_from);
+    if(query.date_to) measurement.dateTo(query.date_to);
+    measurement.sort(-1);
+    if(query.limit) measurement.limit(query.limit);
 
-    result = await db.submit();
+    result = await measurement.submit();
     return result;
 
   }
