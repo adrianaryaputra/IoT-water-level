13f2721937f9f94f3a1696349c48e58ff1809cc4
Adrian A. Firmansyah <adrianaryaputra@icloud.com>
2020-08-30 14:54:24 +0700

    🧪 add more test to post measurement

--- Services/measurement/src/controller/post-measurement.spec.js
+++ Services/measurement/src/controller/post-measurement.spec.js
@@ -1,5 +1,6 @@
 const supertest = require('supertest');
-const {app, server} = require('..')
+const {app, server} = require('..');
+const Config = require('../config');
 
 const {
   DB,
@@ -8,7 +9,7 @@ const {
 
 const fakeMeasurement = require('../../__test__/fixture/fakeMeasurement');
 
-const HTTP_ADDRESS = '/api/v1/measurement';
+const HTTP_ADDRESS = Config.HTTP_ADDRESS;
 
 describe('simulate API POST call', () => {
 
@@ -30,7 +31,7 @@ describe('simulate API POST call', () => {
   it('should be able to add new measurement to DB', async () => {
 
     // generate fake data
-    var mock = fakeMeasurement({timestamp: new Date()});
+    var mock = fakeMeasurement();
     mock.mac_address = mock.mac_address.toUpperCase();
 
     // call POST method
@@ -38,9 +39,69 @@ describe('simulate API POST call', () => {
       .post(HTTP_ADDRESS)
       .send(mock)
       .expect(200)
-      .then(res => {
-        console.log(res.body)
+
+    // check DB 
+    measurementDB().findAll().submit()
+      .then(result => {
+        expect(result.length).toBe(1);
+      })
+
+  });
+
+
+  it('should reject new measurement with no mac_address', async () => {
+
+    // generate fake data
+    var mock = fakeMeasurement({
+      mac_address: undefined,
     });
+
+    // call POST method
+    await supertest(app)
+      .post(HTTP_ADDRESS)
+      .send(mock)
+      .expect(400)
+      .then(response => {
+        expect(response.body.error).toContain('mac_address');
+      })
+
+  });
+  
+  
+  it('should reject new measurement with no lifetime', async () => {
+
+    // generate fake data
+    var mock = fakeMeasurement({
+      lifetime: undefined,
+    });
+
+    // call POST method
+    await supertest(app)
+      .post(HTTP_ADDRESS)
+      .send(mock)
+      .expect(400)
+      .then(response => {
+        expect(response.body.error).toContain('lifetime');
+      })
+
+  });
+
+
+  it('should reject new measurement with no measurement', async () => {
+
+    // generate fake data
+    var mock = fakeMeasurement({
+      measurement: undefined,
+    });
+
+    // call POST method
+    await supertest(app)
+      .post(HTTP_ADDRESS)
+      .send(mock)
+      .expect(400)
+      .then(response => {
+        expect(response.body.error).toContain('measurement');
+      })
 
   });
 
